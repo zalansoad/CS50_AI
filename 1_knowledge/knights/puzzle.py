fb6254683749c836174ae544a9b5d51a3b69e9f8
Zalán Fülöp <zalan12f@gmail.com>
2024-07-16 22:11:21 +0200

    knight

--- 1_knowledge/knights/puzzle.py
+++ 1_knowledge/knights/puzzle.py
@@ -35,7 +35,16 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Or(AKnave, AKnight),
+    Not(And(AKnave, AKnight)),
+    Or(BKnave, BKnight),
+    Not(And(BKnave, BKnight)),
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
+
+
 )
 
 # Puzzle 3
