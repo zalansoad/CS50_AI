025ae558273d50c9fe536b22cdd0a8e2cda4b597
Zalán Fülöp <zalan12f@gmail.com>
2025-03-09 17:13:16 +0100

    get_model

---
+++ 5_neural_networks/traffic/README.md
@@ -0,0 +1,5 @@
+Initially I only used one convolutional and pooling layer. The result was not accurate at all, it was around 5%. 
+I added another layer of convolutional and pooling. This improved the program and the result was more than 94%.
+After this I added the 3rd convolutional and pooling layers. This provided a result of 85% of accuracy.
+I increased the Dense to 256, the result was 93%
+The dropout was reduced to 0.3 and the result was 95.72% Since this is better than the example task I consider this a great result.
--- 5_neural_networks/traffic/traffic.py
+++ 5_neural_networks/traffic/traffic.py
@@ -62,20 +62,21 @@ def load_data(data_dir):
     images = []
     labels = []
     # os.path.join(folder, file)
-    #iterating over the files in the data_dir
+    # iterating over the files in the data_dir
     for category in os.listdir(data_dir):
-        #creating paths for the files
+        # creating paths for the files
         category_path = os.path.join(data_dir, category)
 
-        #iterating over the imgs in the path
+        # iterating over the imgs in the path
         for image in os.listdir(category_path):
             image_path = os.path.join(category_path, image)
+            # open image with cv2
             cvimage = cv2.imread(image_path)
+            # resizing img
             resized_img = cv2.resize(cvimage, (IMG_WIDTH, IMG_HEIGHT))
             images.append(resized_img)
             labels.append(category)
     return (images, labels)
-
 
 
 def get_model():
@@ -84,7 +85,42 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    # Convolutional layer. Learn 32 filters using a 3x3 kernel
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # 2nd Convolutional and Pooling layer
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu"
+        ),
+
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.3),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"])
+
+    return model
 
 
 if __name__ == "__main__":
